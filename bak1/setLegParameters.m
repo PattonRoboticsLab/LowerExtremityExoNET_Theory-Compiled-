0467325b619cae1e8f8b8225853ddc019a192af6
loshea1 <86704069+loshea1@users.noreply.github.com>
2024-04-08 17:20:57 -0500

    Add files via upload

---
+++ bak1/setLegParameters.m
@@ -0,0 +1,297 @@
+function S = setLegParameters(S)
+%Lengths, pose, Mass, RLoHi, thetaLoHi, L0LoHi, LL0LoHi, nParameters, K2j, K1j, K, nElements, springType, nJoints, nTries, AA
+% Create UIFigure and hide until all components are created
+UIFigure = uifigure('Visible', 'off');
+UIFigure.Position = [100 100 530 548];
+UIFigure.Name = 'Leg Parameters';
+
+RLoHimSpinnerLabel = uilabel(UIFigure, "HorizontalAlignment", "right", "Visible", "off", "Position", [68 252 59 22],"Text", "RLoHi [m]");
+RLo = uispinner(UIFigure, "Step", 0.1, "Limits", [0 Inf], "Visible", "off", "Position", [32 221 63 22]);
+RHi = uispinner(UIFigure, "Step", 0.1, "Limits", [0 Inf], "Visible", "off", "Position", [97 221 60 22]);
+
+thetaLoHidegLabel = uilabel(UIFigure, "HorizontalAlignment", "right", "Visible", "off", "Position", [55 180 87 22],"Text", "thetaLoHi [deg]");
+thetaLo = uispinner(UIFigure, "Limits", [-360 360], "Visible", "off", "Position", [32 149 63 22]);
+thetaHi = uispinner(UIFigure, "Limits", [0 360], "Visible", "off", "Position", [97 149 60 22]);
+
+L0LoHimSpinnerLabel = uilabel(UIFigure, "HorizontalAlignment", "right", "Visible", "off", "Position", [67 108 63 22],"Text", "L0LoHi [m]");
+L0Lo = uispinner(UIFigure, "Step", 0.1, "Limits", [0 Inf], "Visible", "off", "Position", [32 77 64 22]);
+L0Hi = uispinner(UIFigure, "Step", 0.1, "Limits", [0 Inf], "Visible", "off", "Position", [98 77 59 22]);
+
+LL0LoHimLabel = uilabel(UIFigure, "HorizontalAlignment", "right", "Visible", "off", "Position", [60 41 70 22],"Text", "LL0LoHi [m]");
+LL0Lo = uispinner(UIFigure, "Limits", [0 Inf], "Visible", "off", "Position", [32 10 64 22], "Step", 0.1);
+LL0Hi = uispinner(UIFigure, "Limits", [0 Inf], "Visible", "off", "Position", [98 10 59 22], "Step", 0.1);
+
+% Create TypeofSpringButtonGroup
+springGroup = uibuttongroup(UIFigure, "SelectionChangedFcn",...
+    @springButtonChange, "Position", [82 448 123 69]);
+springGroup.Title = 'Type of Spring';
+
+uiradiobutton(springGroup, "Text", "hold", "Position", [11 23 45 22],"Visible", "off", "Value", true); %placeholder until something is selected
+CompressionButton = uiradiobutton(springGroup, "Text", "Compression", "Position", [11 22 93 22]);
+TensionButton = uiradiobutton(springGroup, "Text", "Tension", "Position", [11 1 65 22]);
+
+% Create TypeofJointButtonGroup
+jointGroup = uibuttongroup(UIFigure, "SelectionChangedFcn", @jointButtonChange,"Position", [295 449 196 92]);
+jointGroup.Title = 'Type of Joint';
+jointGroup.Visible = 'off';
+
+uiradiobutton(jointGroup, "Text", "hold", "Position", [11 46 45 22], "Value", true,"Visible", "off"); %placeholder until something is selected
+KneeToe = uiradiobutton(jointGroup, "Text", "Only knee-toe", "Position",[11 46 97 22]);
+AnkleToe = uiradiobutton(jointGroup, "Text", "Only ankle-toe", "Position", [11 24 99 22]);
+Both = uiradiobutton(jointGroup, "Text", "Both ankle-toe and knee-toe", "Position", [11 2 173 22]);
+%HipKnee = uiradiobutton(jointGroup, "Text", "Hip-Knee", "Position", [11 3 106 22]);
+
+ofstackedelementsperjointDropDownLabel = uilabel(UIFigure, "HorizontalAlignment", "right", "Visible", "off", "Position", [259 368 171 22],"Text", "# of stacked elements per joint:");
+ofstackedelementsperjointDropDown = uidropdown(UIFigure, "Items", {'1 ', '2', '3', '4', '5', '6'}, "Visible", "off", "Position", [445 368 46 22], "Value", '1 ');
+
+K1jEditFieldLabel  = uilabel(UIFigure, "HorizontalAlignment", "right", "Visible", "off", "Position", [322 326 25 22],"Text", "K1j");
+K1jEditField = uieditfield(UIFigure, 'numeric', "Limits", [0 Inf], "Visible", "off", "Tooltip", {'spring stiffness in [N/m] for a 1-joint compression spring'}, "Position", [362 326 100 22]);
+
+K2jEditFieldLabel  = uilabel(UIFigure, "HorizontalAlignment", "right", "Visible", "off", "Position", [323 292 25 22],"Text", "K2j");
+K2jEditField = uieditfield(UIFigure, 'numeric', "Limits", [0 Inf], "Visible", "off", "Tooltip", {'spring stiffness in [N/m] for a 2-joint compression spring'}, "Position", [363 292 100 22]);
+
+ThighEditFieldLabel  = uilabel(UIFigure, "HorizontalAlignment", "right", "Visible", "off", "Position", [224 156 39 22],"Text", "Thigh");
+ThighLength = uieditfield(UIFigure, 'numeric', "Limits", [0 Inf], "Visible", "off", "Position", [278 156 100 22], "Value", 0.46);
+ThighAngle = uieditfield(UIFigure, 'numeric', "Limits", [0 Inf], "Visible", "off", "Position", [427 156 100 22], "Value", 10);
+
+ShankEditFieldLabel = uilabel(UIFigure, "HorizontalAlignment", "right", "Visible", "off", "Position", [221 125 43 22],"Text", "Shank");
+ShankLength = uieditfield(UIFigure, 'numeric', "Limits", [0 Inf], "Visible", "off", "Position", [279 125 100 22], "Value", 0.42);
+ShankAngle = uieditfield(UIFigure, 'numeric', "Limits", [0 Inf], "Visible", "off", "Position", [427 125 100 22], "Value", 20);
+
+FootEditFieldLabel = uilabel(UIFigure, "HorizontalAlignment", "right", "Visible", "off", "Position", [231 92 33 22],"Text", "Foot");
+FootLength =  uieditfield(UIFigure, 'numeric', "Limits", [0 Inf], "Visible", "off", "Position", [279 92 100 22], "Value", 0.26);
+FootAngle = uieditfield(UIFigure, 'numeric', "Limits", [0 Inf], "Visible", "off", "Position", [428 92 100 22], "Value", 90);
+
+BodyMasskgEditFieldLabel = uilabel(UIFigure, "HorizontalAlignment", "right", "Visible", "off", "Position", [299 231 87 22],"Text", "Body Mass [kg]");
+BodyMasskgEditField = uieditfield(UIFigure, 'numeric', "Limits", [0 Inf], "Visible", "off", "Position", [401 231 100 22], "Value", 70, "Tooltip", {'Body mass for a body height of 1.7 m'});
+
+LengthmLabel = uilabel(UIFigure, "Visible", "off", "Position", [298 189 62 22], "Text", "Length [m]");
+AngledegLabel = uilabel(UIFigure, "Visible", "off", "Position", [443 189 66 22], "Text", "Angle [deg]");
+
+Border = uibutton(UIFigure, 'push', "Visible", "off", "Enable", "off",...
+    "BackgroundColor",[0.8 0.8 0.8], "Tooltip", "These are generic body dimensions for an average man. Values typically do not change. ",...
+    "Position",[221 77 310 184], "Text", " ");
+
+ParametersEditFieldLabel = uilabel(UIFigure, "HorizontalAlignment", "right", "Visible", "off", "Position", [57 309 67 22],"Text", "Parameters");
+ParametersEditField = uieditfield(UIFigure, 'numeric', "Limits", [0 Inf], "Visible", "off", "Tooltip", {'number of parameters for each spring'}, "Position", [139 309 41 22],"Value", 3);
+
+TriesEditFieldLabel = uilabel(UIFigure, "HorizontalAlignment", "right", "Visible", "off", "Position", [82 387 42 22],"Text", "# Tries");
+TriesEditField =  uieditfield(UIFigure, 'numeric', "Limits", [0 Inf], "Visible", "off", "Position", [139 387 41 22],"Value", 50);
+
+KEditFieldLabel = uilabel(UIFigure, "HorizontalAlignment", "right", "Visible", "off", "Position", [322 326 25 22],"Text", "K");
+KEditField = uieditfield(UIFigure, 'numeric', "Limits", [0 Inf], "Visible", "off", "Position", [362 326 100 22]);
+
+AntiAssistanceCheckBox = uicheckbox(UIFigure, "Visible", "off", "Text", "Anti-Assistance", "Position", [91 347 105 22]);
+
+% Create RunButton
+RunButton = uibutton(UIFigure, 'push', 'Text', 'RUN', 'Position', [324 10 100 22]);
+RunButton.BackgroundColor = [0 1 0];
+RunButton.ButtonPushedFcn = @saveAndClose;
+
+% Show the figure after all components are created
+UIFigure.Visible = 'on';
+
+uiwait(UIFigure);
+    function springButtonChange(src, event)
+        if S.case ~= 1.2
+            jointGroup.Visible = 'on';
+        else %if it is hip
+            S.EXONET.nJoints = 3;
+            KEditField.Value = 2000;
+            RLo.Value = 0.001;
+            RHi.Value = 0.16;
+            thetaLo.Value = -360;
+            thetaHi.Value = 360;
+            L0Lo.Value = 0.05;
+            L0Hi.Value = 0.30;
+            LL0Lo.Value = 0.10;
+            LL0Hi.Value = 0.80;
+            S.flag = 2;
+            KEditField.Visible = 'on';
+            KEditFieldLabel.Visible = 'on';
+            visible();
+        end
+    end
+    function jointButtonChange(src, event)
+        if S.case == 1.1
+            RLo.Value = 0.1;
+            RHi.Value = 0.3;
+            thetaLo.Value = 200;
+            thetaHi.Value = 360;
+        elseif S.case == 2 %Gait Stabilizaiton
+            KEditField.Value = 400;
+            RLo.Value = 0.001;
+            RHi.Value = 0.3;
+            thetaLo.Value = -360;
+            thetaHi.Value = 360;
+            L0Lo.Value = 0.05;
+            L0Hi.Value = 0.30;
+            LL0Lo.Value = 0.05;
+            LL0Hi.Value = 0.70;
+        elseif S.case == 3 %Leg optomization
+            RLo.Value = 0;
+            RHi.Value = .3;
+            thetaLo.Value = -360;
+            thetaHi.Value = 360;
+            L0Lo.Value = .4;
+            L0Hi.Value = .70;
+            KEditField.Value = 1000;
+        end
+        
+        %========================================================%
+        if TensionButton.Value == true
+            LL0Lo.Value = 0.05;
+            LL0Hi.Value = 0.2;
+            K1jEditField.Value = 2000;
+            K2jEditField.Value = 800;
+            L0Lo.Value = 0.05;
+            L0Hi.Value = 0.20;
+        elseif CompressionButton.Value == true
+            LL0Lo.Value = 0.80;
+            LL0Hi.Value = 1;
+            K1jEditField.Value = 600;
+            K2jEditField.Value = 600;
+            L0Lo.Value = 0.10;
+            L0Hi.Value = 0.20;
+        end
+        %========================================================%
+        
+        if event.NewValue == Both
+            S.EXONET.nJoints = 2;
+            K1jEditFieldLabel.Visible = 'on';
+            K1jEditField.Visible = 'on';
+            K2jEditFieldLabel.Visible = 'on';
+            K2jEditField.Visible = 'on';
+            KEditField.Visible = 'off';
+            KEditFieldLabel.Visible = 'off';
+            S.flag = [0,1];
+        elseif event.NewValue == KneeToe
+            S.EXONET.nJoints = 22;
+            K2jEditField.Value = 1000;
+            RLo.Value = 0.03;
+            thetaLo.Value = 0;
+            L0Lo.Value = 0.40;
+            L0Hi.Value = 0.50;
+            
+            KEditField.Visible = 'off';
+            KEditFieldLabel.Visible = 'off';
+            K1jEditFieldLabel.Visible = 'on';
+            K1jEditField.Visible = 'on';
+            K2jEditFieldLabel.Visible = 'on';
+            K2jEditField.Visible = 'on';
+            S.flag = 1;
+        elseif event.NewValue == AnkleToe
+            S.EXONET.nJoints = 11;
+            K1jEditFieldLabel.Visible = 'on';
+            K1jEditField.Visible = 'on';
+            K2jEditFieldLabel.Visible = 'on';
+            K2jEditField.Visible = 'on';
+            
+            KEditField.Visible = 'off';
+            KEditFieldLabel.Visible = 'off';
+            S.flag = 0;
+        end
+        visible();
+    end
+        function visible()
+            AntiAssistanceCheckBox.Visible = 'on';
+            TriesEditFieldLabel.Visible = 'on';
+            TriesEditField.Visible = 'on';
+            LL0Hi.Visible = 'on';
+            L0LoHimSpinnerLabel.Visible = 'on';
+            RLoHimSpinnerLabel.Visible = 'on';
+            RHi.Visible = 'on';
+            RLo.Visible = 'on';
+            thetaLo.Visible = 'on';
+            thetaLoHidegLabel.Visible = 'on';
+            thetaHi.Visible = 'on';
+            L0Lo.Visible = 'on';
+            L0LoHimSpinnerLabel.Visible = 'on';
+            L0Hi.Visible = 'on';
+            ofstackedelementsperjointDropDownLabel.Visible = 'on';
+            ofstackedelementsperjointDropDown.Visible = 'on';
+            ThighEditFieldLabel.Visible = 'on';
+            ThighLength.Visible = 'on';
+            ShankEditFieldLabel.Visible = 'on';
+            ShankLength.Visible = 'on';
+            FootEditFieldLabel.Visible = 'on';
+            FootLength.Visible = 'on';
+            BodyMasskgEditFieldLabel.Visible = 'on';
+            BodyMasskgEditField.Visible = 'on';
+            ThighAngle.Visible = 'on';
+            ShankAngle.Visible = 'on';
+            FootAngle.Visible = 'on';
+            LengthmLabel.Visible = 'on';
+            AngledegLabel.Visible = 'on';
+            ParametersEditFieldLabel.Visible = 'on';
+            ParametersEditField.Visible = 'on';
+            LL0Lo.Visible = 'on';
+            LL0LoHimLabel.Visible = 'on';
+            LL0Hi.Visible = 'on';
+            RLoHimSpinnerLabel.Visible = 'on';
+            RHi.Visible = 'on';
+            RLo.Visible = 'on';
+            thetaLo.Visible = 'on';
+            thetaLoHidegLabel.Visible = 'on';
+            thetaHi.Visible = 'on';
+            L0Lo.Visible = 'on';
+            L0LoHimSpinnerLabel.Visible = 'on';
+            L0Hi.Visible = 'on';
+            ofstackedelementsperjointDropDownLabel.Visible = 'on';
+            ofstackedelementsperjointDropDown.Visible = 'on';
+            ThighEditFieldLabel.Visible = 'on';
+            ThighLength.Visible = 'on';
+            ShankEditFieldLabel.Visible = 'on';
+            ShankLength.Visible = 'on';
+            FootEditFieldLabel.Visible = 'on';
+            FootLength.Visible = 'on';
+            BodyMasskgEditFieldLabel.Visible = 'on';
+            BodyMasskgEditField.Visible = 'on';
+            ThighAngle.Visible = 'on';
+            ShankAngle.Visible = 'on';
+            FootAngle.Visible = 'on';
+            LengthmLabel.Visible = 'on';
+            AngledegLabel.Visible = 'on';
+            ParametersEditFieldLabel.Visible = 'on';
+            ParametersEditField.Visible = 'on';
+            LL0Lo.Visible = 'on';
+            LL0LoHimLabel.Visible = 'on';
+            LL0Hi.Visible = 'on';
+            Border.Visible = 'on';
+        end
+% Callback function for the "Run" button
+    function saveAndClose(~, ~)
+        % Get the updated values from UI components
+        % Lengths, pose, Mass, RLoHi, thetaLoHi, L0LoHi, LL0LoHi, nParameters, K2j, K1j, K, nElements, springType, nJoints, nTries, AA
+        S.BODY.Lengths = [ThighLength.Value, ShankLength.Value, FootLength.Value];
+        S.BODY.pose = [ThighAngle.Value, ShankAngle.Value, FootAngle.Value];
+        S.BODY.Mass = BodyMasskgEditField.Value;
+        
+        S.RLoHi = [RLo.Value RHi.Value];
+        S.thetaLoHi = [thetaLo.Value thetaHi.Value];
+        S.L0LoHi = [L0Lo.Value L0Hi.Value];
+        S.LL0LoHi = [LL0Lo.Value LL0Hi.Value]; %for 2-joint
+        
+        S.AA = AntiAssistanceCheckBox.Value;
+        S.nTries = TriesEditField.Value;
+        
+        S.EXONET.nParameters = ParametersEditField.Value;
+        S.EXONET.nElements = str2double(ofstackedelementsperjointDropDown.Value);
+        
+        if S.EXONET.nJoints ~= 3
+            S.EXONET.K2j = K2jEditField.Value;
+            S.EXONET.K1j = K1jEditField.Value;
+        else
+            S.EXONET.K = KEditField.Value; %spring stiffness for hip
+        end
+        
+        if CompressionButton.Value == true
+            S.Spring = 1;
+        elseif TensionButton.Value == true
+            S.Spring = 2;
+        end
+        % Close the UI figure
+        close(UIFigure);
+    end
+
+end
---
+++ bak1/setUpLeg.m
@@ -0,0 +1,161 @@
+% ***********************************************************************
+% SETUP:
+% Set the parameters for the Body and for the ExoNET
+% ***********************************************************************
+function S = setUpLeg(S)
+%% BEGIN
+% Define Variables
+S = setLegParameters(S);
+
+%Colors for graphics
+S.ColorsS = [0.5 0.7 1; 0.1 1 0.2; 1 0.6 0.3]; % 3 distinct RGB color spaces for the springs
+S.ColorsR = [0 0.2 0.9; 0 0.7 0; 0.9 0.4 0]; % 3 distinct RGB color spaces for the rotators
+S.ColorsY = [1 0.87843 0.40]; % yellow springs
+S.ColorsC = [0.0235294 0.8392157 0.627451]; % green 1-joint springs
+S.ColorsT = [1 0.572549 0.545098]; % pink 2-joint springs
+S.LWs = 2; % lines width springs
+S.LWr = 4; % lines width rotators
+
+%% EXONET
+i = 0;
+if S.EXONET.nJoints == 11 || S.EXONET.nJoints == 22 %knee-ankle or ankle-toe
+    const = 1;
+elseif S.EXONET.nJoints == 2 %both knee and ankle
+    const = 2;
+elseif S.EXONET.nJoints == 3 %hip
+    const = S.EXONET.nJoints;
+end
+
+S.EXONET.pConstraint = NaN*zeros(const*S.EXONET.nElements*S.EXONET.nParameters,2); % initialization
+for joint = 1:const
+    for element = 1:S.EXONET.nElements
+            i = i+1;
+            S.EXONET.pConstraint(i,:) = S.RLoHi;
+            i = i+1;
+            S.EXONET.pConstraint(i,:) = S.thetaLoHi;
+            i = i+1;
+           S.EXONET.pConstraint(i,:) = S.L0LoHi;
+    end
+end
+
+if S.case == 1.1 
+    I = S.EXONET.nParameters*S.EXONET.nElements+3;
+    for j = I:3:length(S.EXONET.pConstraint)
+        S.EXONET.pConstraint(j,:) = S.LL0LoHi;
+    end
+elseif S.case == 2 %gait stabilize
+    I = (9*S.EXONET.nElements)-3*(S.EXONET.nElements-1);
+    for j = I:3:length(S.EXONET.pConstraint)
+        S.EXONET.pConstraint(j,:) = S.LL0LoHi;
+    end
+end
+%% IMPORT DATA OF THE WALK CYCLE
+beatrice_gait_hip_knee_ankle = xlsread('beatrice_gait_hip_knee_ankle.xlsx'); % walk cycle parameters for the right leg
+                                                  % (from Bovi et al.)
+S.percentCycle = beatrice_gait_hip_knee_ankle(:,1); % percentage of gait cycle
+S.Hip.Angle = beatrice_gait_hip_knee_ankle(:,2);    % hip angles in [deg]
+S.Knee.Angle = beatrice_gait_hip_knee_ankle(:,3);   % knee angles in [deg]
+S.Ankle.Angle = beatrice_gait_hip_knee_ankle(:,4);  % ankle angles in [deg]
+S.Hip.Moment = beatrice_gait_hip_knee_ankle(:,5);   % hip moments of force in [Nm/kg]
+S.Knee.Moment = beatrice_gait_hip_knee_ankle(:,6);  % knee moments of force in [Nm/kg]
+S.Ankle.Moment = beatrice_gait_hip_knee_ankle(:,7); % ankle moments of force in [Nm/kg]
+
+phis = [S.Hip.Angle, S.Hip.Angle+S.Knee.Angle, 90-(S.Knee.Angle+S.Ankle.Angle)]; % angles in [deg]
+tausD = S.Ankle.Moment.*(-1).*S.BODY.Mass; % ankle moments of force in [Nm]
+tausKnee = S.Knee.Moment.*S.BODY.Mass; % knee moments of force in [Nm]
+
+% ENTIRE GAIT CYCLE
+%S.PHIs = [S.Hip.Angle, S.Hip.Angle+S.Knee.Angle, 90-(S.Knee.Angle+S.Ankle.Angle)]; % angles in [deg]
+%S.TAUsDESIRED = S.Ankle.Moment.*(-1).*S.BODY.Mass; % moments of force in [Nm]
+
+% STANCE PHASE
+%S.PHIs = [S.Hip.Angle(1:63), S.Hip.Angle(1:63)+S.Knee.Angle(1:63), 90-(S.Knee.Angle(1:63)+S.Ankle.Angle(1:63))]; % angles in [deg]
+%S.TAUsDESIRED = S.Ankle.Moment(1:63).*(-1).*S.BODY.Mass; % moments of force in [Nm]
+
+% LATE STANCE PHASE
+if S.case ~= 2
+S.PHIs = [S.Hip.Angle(9:63), S.Hip.Angle(9:63)+S.Knee.Angle(9:63), 90-(S.Knee.Angle(9:63)+S.Ankle.Angle(9:63))]; % angles in [deg]
+S.TAUsDESIRED = [S.Ankle.Moment(9:63).*(-1).*S.BODY.Mass, S.Knee.Moment(9:63).*S.BODY.Mass]; % moments of force in [Nm]
+else
+S.PHIs = [S.Hip.Angle, S.Hip.Angle+S.Knee.Angle]; % angles in [deg]
+S.TAUsDESIRED = [S.Hip.Moment.*(-1).*S.BODY.Mass, S.Knee.Moment.*S.BODY.Mass]; % moments of force in [Nm]
+end
+%
+% Hip Flexion (+) Hip Extension (-)
+% Knee Extension (+) Knee Flexion (-)
+% Ankle Dorsiflexion (+) Ankle Plantarflexion (-)
+
+% 4 PERIODS OF STANCE PHASE
+% Initial contact (0%–2% of the Gait Cycle)
+% Loading response (2%–12% of the Gait Cycle)
+% Midstance (12%–31% of the Gait Cycle)
+% Preswing (50%–60% of the Gait Cycle)
+
+%   o HIP
+%   .\
+%   . \
+%   .  \
+%   .   \
+%   .    \
+%   .phi1 \
+%          \
+%           o KNEE
+%          / .
+%         /   .
+%        /     .
+%       /  phi2 .
+%      /
+%     /
+%    / ANKLE
+%   o---------o TOE
+%   .
+%   . phi3
+%   .
+%   .
+
+
+%%
+if S.case == 1.1 || S.case == 1.2
+    % CREATE THE DIFFERENT POSITIONS OF THE BODY
+    S = forwardKinLeg(S); % positions associated to the angles
+elseif S.case == 2
+    % CREATE GRID IN THE ANGLE PLANE
+    Xgrid = -15:5:45;
+    Ygrid = -5:5:95;
+    
+    n = 0;
+    for j = 1:length(Ygrid)
+        for i = 1:length(Xgrid)
+            S.GridPHIs(i+n,:) = [Xgrid(i) Ygrid(j)];
+        end
+        n = n + length(Xgrid);
+    end
+    
+    figure
+    plot(S.GridPHIs(:,1),S.GridPHIs(:,2),'.r')
+    
+end
+%% HANDLE = @(ARGLIST) EXPRESSION   constructs an anonymous function and returns the handle to it
+if S.case == 1.1 %ankle
+    if S.Spring == 1 %compression
+        S.TENSION1j = @(L0,L)   (S.EXONET.K1j.*(L-L0)).*((L-L0)<0).*((L*L0)>0); % 1-joint Linear Compression Springs
+        S.TENSION2j = @(L0,L)   (S.EXONET.K2j.*(L-L0)).*((L-L0)<0).*((L*L0)>0); % 2-joint Linear Compression Springs
+    elseif S.Spring == 2 %tension
+        S.TENSION1j = @(L0,L)   (S.EXONET.K1j.*(L-L0)).*((L-L0)>0).*((L*L0)>0); % 1-joint Linear Tension Springs
+        S.TENSION2j = @(L0,L)   (S.EXONET.K2j.*(L-L0)).*((L-L0)>0).*((L*L0)>0); % 2-joint Linear Tension Springs
+    end
+elseif S.case == 1.2 || S.case == 2 %hip or gait stabilize
+    if S.Spring == 1 %compression
+        S.TENSION = @(L0,L)   (S.EXONET.K.*(L-L0)).*((L-L0)<0).*((L*L0)>0); % (inlineFcn) + stretch
+    elseif S.Spring == 2 %tension
+        S.TENSION = @(L0,L)   (S.EXONET.K.*(L-L0)).*((L-L0)>0).*((L*L0)>0); % (inlineFcn) + stretch
+    end
+end
+%% Optimization Parameters
+optOptions = optimset();
+optOptions.MaxIter = 1E3;     % optimization limit
+optOptions.MaxFunEvals = 1E3; % optimization limit
+S.optOptions = optimset(optOptions);
+       
+fprintf('\n\n\n\n Parameters set~~\n')
+end
